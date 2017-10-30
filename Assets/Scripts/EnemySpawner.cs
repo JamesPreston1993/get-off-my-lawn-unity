076b80f17fbd88cb6fcbe3aab17d840436550511
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-30 13:03:23 +0000

    Add enemy spawner script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnRate;
+    public float xMin;
+    public float xMax;
+
+    private float nextSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+
+            Vector3 position = new Vector3
+            {
+                x = Random.Range(-1 * xMax, xMax),
+                y = transform.position.y,
+                z = transform.position.z
+            };
+            Instantiate(enemy, position, Quaternion.identity);
+        }
+    }
+}

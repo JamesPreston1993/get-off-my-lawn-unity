0d7432d9bf5f5f2cd044b06acc9eb97aabf27e1c
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-30 13:37:44 +0000

    Add shooting controller to player

---
+++ Assets/Scripts/PlayerShootingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+    public GameObject shot;
+    public float shootRate;
+    private float nextShot;
+
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.Space) && Time.time > nextShot)
+        {
+            nextShot = Time.time + shootRate;
+            Instantiate(shot, new Vector3
+            {
+                x = transform.position.x,
+                y = transform.position.y,
+                z = transform.position.z + 2
+            }, Quaternion.identity);
+        }
+	}
+}

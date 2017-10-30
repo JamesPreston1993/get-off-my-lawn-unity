59a1be3ad0c16d6267f741a263f4d7cfa0c5c0f1
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-30 14:49:49 +0000

    Stop player movement and shooting on game over

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,10 +5,31 @@ public class PlayerMovementController : MonoBehaviour
 {
     public float speed;
     public Boundary boundary;
-		
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey("a"))
+
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject parentGameObject = GameObject.FindGameObjectWithTag("GameController");
+        if (parentGameObject != null)
+        {
+            gameController = parentGameObject.GetComponent<GameController>();
+        }
+
+        if (gameController == null)
+        {
+            Debug.Log("GameController not found");
+        }
+    }
+
+    void Update ()
+    {
+        if (gameController.IsGameOver())
+        {
+            return;
+        }
+
+        if (Input.GetKey("a"))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
--- Assets/Scripts/PlayerShootingController.cs
+++ Assets/Scripts/PlayerShootingController.cs
@@ -8,8 +8,29 @@ public class PlayerShootingController : MonoBehaviour
     public float shootRate;
     private float nextShot;
 
-	void Update ()
+    private GameController gameController;
+
+    void Start()
     {
+        GameObject parentGameObject = GameObject.FindGameObjectWithTag("GameController");
+        if (parentGameObject != null)
+        {
+            gameController = parentGameObject.GetComponent<GameController>();
+        }
+
+        if (gameController == null)
+        {
+            Debug.Log("GameController not found");
+        }
+    }
+
+    void Update ()
+    {
+        if (gameController.IsGameOver())
+        {
+            return;
+        }
+
 		if (Input.GetKey(KeyCode.Space) && Time.time > nextShot)
         {
             nextShot = Time.time + shootRate;

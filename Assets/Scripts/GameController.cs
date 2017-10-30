8436033b26fdba25a6522ab063416e31d28bd7e0
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-30 14:39:19 +0000

    Add game over text

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public Text scoreText;
     private int score;
+
+    public GameObject gameOverPanel;
+    private bool gameOver;
 
     void Start()
     {
@@ -22,5 +23,16 @@ public class GameController : MonoBehaviour
     public void UpdateScore()
     {        
         scoreText.text = "Score: " + score;
+    }
+
+    public void SetGameOver(bool isGameOver)
+    {
+        gameOver = isGameOver;
+        gameOverPanel.SetActive(isGameOver);
+    }
+
+    public bool IsGameOver()
+    {
+        return gameOver;
     }
 }

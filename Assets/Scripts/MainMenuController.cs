48af4e857e6caf76b30b0f09bc4d72679684b3af
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-31 20:19:19 +0000

    Implement menu system

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,10 +7,18 @@ public class MainMenuController : MonoBehaviour
     public Button playButton;
     public Button quitButton;
 
+    public Button howToButton;
+    public Button backButton;
+
+    public GameObject mainPanel;
+    public GameObject howToPanel;
+
     void Start ()
     {
         playButton.onClick.AddListener(PlayGame);
         quitButton.onClick.AddListener(QuitGame);
+        howToButton.onClick.AddListener(ShowHowToMenu);
+        backButton.onClick.AddListener(BackToMainMenu);
     }
 
     void PlayGame()
@@ -21,5 +29,17 @@ public class MainMenuController : MonoBehaviour
     void QuitGame()
     {
         Application.Quit();
+    }
+
+    void ShowHowToMenu()
+    {
+        mainPanel.SetActive(false);
+        howToPanel.SetActive(true);
+    }
+
+    void BackToMainMenu()
+    {
+        mainPanel.SetActive(true);
+        howToPanel.SetActive(false);
     }
 }

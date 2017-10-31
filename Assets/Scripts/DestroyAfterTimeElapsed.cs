1cd0930ae295c22ad5966363276fb18b5a639345
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-31 20:42:07 +0000

    Add script to destroy particles after 2 seconds

---
+++ Assets/Scripts/DestroyAfterTimeElapsed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DestroyAfterTimeElapsed : MonoBehaviour
+{
+    public float timeToLive;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update ()
+    {
+	    if (Time.time > startTime + timeToLive)
+        {
+            Destroy(gameObject);
+        }
+	}
+}

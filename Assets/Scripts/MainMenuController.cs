3c36ad2420a58c64d496e6c43e42ffa579a4c569
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-31 19:53:26 +0000

    Add controller for menu buttons

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+
+    void Start ()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}

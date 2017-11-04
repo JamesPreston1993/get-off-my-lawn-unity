9c2574e618abf67f5dfd5d7ae4570452980ea69d
JamesPreston1993 <jamescpreston93@gmail.com>
2017-11-04 17:47:31 +0000

    Update enemy spawn speed over time

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,15 +3,21 @@
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemy;
-    public float spawnRate;
+    public float initialSpawnRate;
+    public float spawnRateUpdateInterval;
+    public float spawnRateAmount;
     public float xMin;
     public float xMax;
 
     private float nextSpawn;
+    private float spawnRate;
+    private float nextIntervalUpdate;
     private GameController gameController;
 
     void Start()
     {
+        spawnRate = initialSpawnRate;
+        nextIntervalUpdate = spawnRateUpdateInterval;
         GameObject parentGameObject = GameObject.FindGameObjectWithTag("GameController");
         if (parentGameObject != null)
         {
@@ -26,22 +32,30 @@ public class EnemySpawner : MonoBehaviour
 
     void Update()
     {
-        if (!gameController.IsGameOver() && Time.time > nextSpawn)
+        if (!gameController.IsGameOver())
         {
-            nextSpawn = Time.time + spawnRate;
+            if (Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnRate;
 
-            Vector3 position = new Vector3
+                Vector3 position = new Vector3
+                {
+                    x = Random.Range(xMin, xMax),
+                    y = 0.01f,
+                    z = transform.position.z
+                };
+                Instantiate(enemy, position, new Quaternion
+                {
+                    x = 0.0f,
+                    y = 180.0f,
+                    z = 0.0f
+                });
+            }
+            if (Time.time > nextIntervalUpdate && spawnRate >= 1)
             {
-                x = Random.Range(xMin, xMax),
-                y = 0.01f,
-                z = transform.position.z
-            };
-            Instantiate(enemy, position, new Quaternion
-            {
-                x = 0.0f,
-                y = 180.0f,
-                z = 0.0f
-            });
+                nextIntervalUpdate = Time.time + spawnRateUpdateInterval;
+                spawnRate -= spawnRateAmount;
+            }
         }
     }
 }
